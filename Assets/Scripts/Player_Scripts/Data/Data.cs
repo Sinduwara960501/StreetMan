802236eb47aa0bb11ce1cdc761b53d4c0d691cc0
Sinduwara960501 <sinduwaranamawardane@gmail.com>
2025-07-10 19:02:27 +0530

    Update statemachine
    update state machine and implement input system.

--- Assets/Scripts/Player_Scripts/Data/Data.cs
+++ Assets/Scripts/Player_Scripts/Data/Data.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "newPlayerData", menuName = "Data/PlayerData/Base Data")]
 public class Data : ScriptableObject
 {
+    [Header("Movement State")]
+    public float Velocity = 5f;
 
 }
--- Assets/Scripts/Player_Scripts/StateMachine/States/SubStates/PlayerMoveState.cs
+++ Assets/Scripts/Player_Scripts/StateMachine/States/SubStates/PlayerMoveState.cs
@@ -6,4 +6,28 @@ public class PlayerMoveState : PlayerGroundedState
     {
 
     }
+    public override void Enter()
+    {
+        base.Enter();
+    }
+    public override void Exit()
+    {
+        base.Exit();
+    }
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (input.x == 0)
+        {
+            playerStateMachine.ChangeState(player.playerIdelState);
+        }
+    }
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+    public override void DoCheck()
+    {
+        base.DoCheck();
+    }
 }
